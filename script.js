49de2a1f64373e755435f8c5c95f89fb17453dfa
tac3563 <tcox_bu@outlook.com>
2021-12-05 22:23:54 +0000

    made the site responsive

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-btn')[0];
 
+const navbarLinks = document.getElementsByClassName('right-nav')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
