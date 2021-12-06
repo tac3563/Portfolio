3e63db00020f369f8d7a95d2a195579b92abe2db
tac3563 <tcox_bu@outlook.com>
2021-12-06 10:29:02 +0000

    added jquery for smoother scroll animation

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+// Nav Toggle
+
 const toggleButton = document.getElementsByClassName('toggle-btn')[0];
 
 const navbarLinks = document.getElementsByClassName('right-nav')[0];
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
+});
+
+// Scroll Animations
+
+$('.back-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+});
+$('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate(
+            {
+                scrollTop: target.offset().top
+            },
+            1000
+        );
+    }
 });
